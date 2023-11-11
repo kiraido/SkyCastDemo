df97f3222164ea28aab91d00e87bdff6c0f2ac3c
leikai <1328156462@qq.com>
2023-11-11 18:12:26 +0800

    第二次提交

--- src/main/java/com/example/skycastdemo/Service/WeatherService.java
+++ src/main/java/com/example/skycastdemo/Service/WeatherService.java
@@ -13,31 +13,6 @@ public class WeatherService {
         this.restTemplate = restTemplate;
     }
 
-    public String[] getRegionCountry() {
-        String url = baseUrl + "/getRegionCountry";
-        return restTemplate.getForObject(url, String[].class);
-    }
-
-    public String getRegionDataset() {
-        String url = baseUrl + "/getRegionDataset";
-        return restTemplate.getForObject(url, String.class); // Assuming the response is in String format
-    }
-
-    public String[] getRegionProvince() {
-        String url = baseUrl + "/getRegionProvince";
-        return restTemplate.getForObject(url, String[].class);
-    }
-
-    public String getSupportCityDataset(String regionCode) {
-        String url = baseUrl + "/getSupportCityDataset?theRegionCode=" + regionCode;
-        return restTemplate.getForObject(url, String.class); // Assuming the response is in String format
-    }
-
-    public String[] getSupportCityString(String regionCode) {
-        String url = baseUrl + "/getSupportCityString?theRegionCode=" + regionCode;
-        return restTemplate.getForObject(url, String[].class);
-    }
-
     public String[] getWeather(String cityCode) {
         String url = baseUrl + "/getWeather?theCityCode=" + cityCode + "&theUserID=";
         return restTemplate.getForObject(url, String[].class);
